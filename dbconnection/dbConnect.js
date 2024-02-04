f66cd3f90f1db7e00df1f233bbf3e0eb36dc115f
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2024-02-04 23:32:17 +0100

    push edited copy

--- dbconnection/dbConnect.js
+++ dbconnection/dbConnect.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const dbconnect = async() =>{
     try {
-        const connect = await mongoose.connect("mongodb+srv://Habeeb:Ademola1234@habeeb.pal57xa.mongodb.net/PowerBall?retryWrites=true&w=majority")
+        // const connect = await mongoose.connect("mongodb+srv://Habeeb:Ademola1234@habeeb.pal57xa.mongodb.net/PowerBall?retryWrites=true&w=majority")
+        const connect = await mongoose.connect("mongodb+srv://pbllworld:goal12345@powerball.lllsua4.mongodb.net/powerball?retryWrites=true&w=majority")
         if(connect){
             console.log("successfully connected to the database")
         } else {
